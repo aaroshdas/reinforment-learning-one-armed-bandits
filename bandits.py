f4bf16005e1e182f8f543492b3a73b809ee32099
Aarosh Das <aaroshdas@gmail.com>
2025-01-29 13:50:00 -0500

    ucb

--- bandits.py
+++ bandits.py
@@ -1,4 +1,5 @@
 import random
+import math
 def generate_bandits():
     bandits = []
     for i in range(10):
@@ -25,15 +26,40 @@ def play_game(bandits, epsilonVal):
             qVals[index] = banditSums[index]/banditCount[index]
     return sum(banditSums)
 
+def play_game_with_uncertaintiy(bandits):
+    banditCount = [0]*10
+    banditSums = [0]*10
+    uncertanties = [1]*10 #I START UNCERTANTIES AT 1 CAUSE IT JUST ACTS NORMAL GREEDY AT 0
+    qVals = [0]*10
+    for i in range(2000):
+        index = uncertanties.index(max(uncertanties))
+        banditSums[index] += random.normalvariate(bandits[index], 1)
+        banditCount[index] +=1
+        qVals[index] = banditSums[index]/banditCount[index]
+        uncertanties[index] = qVals[index]+ (math.log(sum(banditCount))/(banditCount[index]+1e-10))**0.5
+    return sum(banditSums)
+
+
 scores= [0]*len(epsilonVals)
 bandits = generate_bandits()
+cheat_score = 0
+for i in range(2000):
+    cheat_score += random.normalvariate(max(bandits), 1)
+
+print("cheating score", cheat_score)
 for k in range(len(epsilonVals)):
     for i in range(200):
         scores[k] += play_game(bandits, epsilonVals[k])
 
 for i in range(len(scores)):
     scores[i] = scores[i]/200
-print(scores)
+    print(str(epsilonVals[i]) +"- ", scores[i])
+print("highest score epsilon val " + str(epsilonVals[scores.index(max(scores))]) +"-", max(scores))
+
+ucb_score = 0
+for i in range(200):
+    ucb_score += play_game_with_uncertaintiy(bandits)
+print("ucb score", ucb_score/200, ", "+str(round(100-(ucb_score/(200*cheat_score))*100,2)) + "% difference")
 
 
 #play game with uncertainty -  make temp list add uncertanties, play based on max uncertain no epislons just greed
